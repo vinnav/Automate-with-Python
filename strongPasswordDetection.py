93a01a1545a53dc30bfcd291daccb8a9fcd539d6
vinnav <54999963+vinnav@users.noreply.github.com>
2019-11-02 10:22:59 +0000

    Create strongPasswordDetection.py

---
+++ strongPasswordDetection.py
@@ -0,0 +1,24 @@
+import re
+
+# Write a function that uses regular expressions to make sure the password
+# string it is passed is strong. 
+# A strong password is defined as one that is:
+# - at least eight characters long,
+# - contains both uppercase and lowercase characters,
+# - has at least one digit. 
+
+
+strongPassword = re.compile(r'(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})')
+
+def checkPasswordStrength(pwd):
+    if re.match(strongPassword, pwd):
+        return True
+    else:
+        return False
+
+pwd = input("Enter your password: ")
+
+if checkPasswordStrength(pwd):
+    print("Your password is strong")
+else:
+    print("Your password is weak, it needs to contain 8 characters, at least a lower case, upper case and a number")
