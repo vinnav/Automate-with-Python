495288158734930840fe0a310885c89ee339ba19
vinnav <54999963+vinnav@users.noreply.github.com>
2019-11-04 23:02:17 +0000

    Update requests.py

--- 11-webScraping/requests.py
+++ 11-webScraping/requests.py
@@ -11,3 +11,11 @@ type(res)
 res.status_code == requests.codes.ok
 len(res.text)
 print(res.text[:250])
+
+# Download the file to RomeoAndJuliet.txt in cwd
+
+playFile = open('RomeoAndJuliet.txt', 'wb')
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+
+playFile.close()
