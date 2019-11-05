76f4482bd10bc66c658a1d134bce5270d66a5425
vinnav <54999963+vinnav@users.noreply.github.com>
2019-11-05 18:04:55 +0000

    create downloadXkcd.py

--- 11-webScraping/lucky.py
+++ 11-webScraping/lucky.py
@@ -7,11 +7,13 @@ print('Googling...')    # display text while dowloading the Google page
 res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
-# TODO: Retrieve top search result links.
-soup = bs4.BeautifulSoup(res.text)
 
+# TODO: Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text, "lxml")
+# print(soup.select("a"))
 # TODO: Open a browser tab for each result.
-linkElems = soup.select('.r a')
+linkElems = soup.select(".r a")
+
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
     webbrowser.open('http://google.com' + linkElems[i].get('href'))
